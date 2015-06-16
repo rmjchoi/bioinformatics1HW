40429b666bff74aac15ee5674538fa8fac0d5c25
mauricio <machalita@yahoo.com>
2015-06-16 13:21:31 +0900

    class  update for sequence new class for matrix

--- classes.py
+++ classes.py
@@ -10,7 +10,26 @@ class sequence:
     def get_length(self):
         return len(self.sequence)
 
-    #Add char function
+    def set_sequence(self, seq):
+        self.sequence = seq
 
-    #Set sequence function
+    def add_char(self, char):
+        newseq = char + self.get_sequence()
+        self.set_sequence(newseq)
 
+class matrix:
+    def __init__(self, width, height, match, mismatch, gap):
+        self.matrix = [[0 for i in range(width + 1)] for j in range(height + 1)]
+        self.matchscore = match
+        self.mismatchscore = mismatch
+        self.gapscore = gap
+
+    def get_position(self, x, y):
+        return self.matrix[x][y]
+
+    def set_position(self,x,y,value):
+        self.matrix[x][y] = value
+
+    def print_matrix(self):
+        for row in self.matrix:
+            print row
