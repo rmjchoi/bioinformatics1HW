f1f2860ff55aab1dd41508b04f93d91968288f14
rmjchoi <rmjchoi@gmail.com>
2015-06-16 14:35:00 +0900

    calculate matrix

--- main.py
+++ main.py
@@ -3,19 +3,12 @@ from classes import sequence
 from classes import matrix
 
 #Main
-sequence1 = sequence("GCATGCUT")
+sequence1 = sequence("GCATGCU")
 sequence2 = sequence("GATTACA")
 
-matrix = matrix(sequence1.get_length(), sequence2.get_length(),1,0,-1)
-colLen = sequence1.get_length()
-rowLen = sequence2.get_length()
-colChar = sequence1.get_sequence()
-rowChar = sequence2.get_sequence()
+matrix = matrix(sequence1.get_length(), sequence2.get_length(),1,-1,-1)
 
 #We initialize the matrix that will hold the scores
-
-
-
 #Create recursive function for matrix initialization with gap values on the first x and y row
 #Recursive format
 def initMatrix(seq1, seq2, r, c):
@@ -25,17 +18,31 @@ def initMatrix(seq1, seq2, r, c):
         initMatrix(seq1,seq2,r,c+1)
         initMatrix(seq1,seq2,r+1,c)
 
-    elif c < colLen+1 and r == 0:
+    elif c < sequence1.get_length()+1 and r == 0:
         matrix.set_position(r,c,matrix.get_position(r,c-1) + matrix.get_gapscore())
        # matrix[r][c] = matrix[r][c-1]+ matrix.get_gapscore()
         c=c+1
         initMatrix(seq1,seq2,r,c)
 
-    elif r < rowLen+1 and c == 0:
+    elif r < sequence2.get_length()+1 and c == 0:
         matrix.set_position(r,c,matrix.get_position(r-1,c) + matrix.get_gapscore())
         # matrix[r][c] = matrix [r-1][c]+gap
         r = r+1
         initMatrix(seq1,seq2,r,c)
+
+    # Calculate
+    elif r<=  sequence2.get_length()+1 and c <= sequence1.get_length()+1:
+        calculateMatrix(seq1,seq2,0,0)
+
+def calculateMatrix (seq1, seq2, r, c):
+    if c < sequence1.get_length() :
+        matrix.set_score(sequence1.get_char(c), sequence2.get_char(r), r, c)
+        c = c + 1
+        calculateMatrix(seq1, seq2, r, c)
+    else:
+        r= r+1
+        if r < sequence2.get_length():
+            calculateMatrix(seq1, seq2, r, 0)
 
 # Initialize matrix w/o recursive
 #     matrix[0][0] = 0
@@ -45,23 +52,11 @@ def initMatrix(seq1, seq2, r, c):
 #
 #     for r in range(1,rowLen+1):
 #             matrix[r][0] = matrix[r-1][0]+gap
-initMatrix(colChar,rowChar,0,0)
+
 
 #Call matrix scoring recursive function
 
-#Create recursive function for matrix scoring
-# def calculateMatrix (seq1, seq2, r, c):
-#     scoreMax = 0
-#     for s in sequence1:
-#         print s
-#     for c in range(1,colLen+1):
-#         for r in range(1,rowLen+1):
-#             if colChar == rowChar:
-#                 scoreMax = match
-#             elif colChar != rowChar:
-#                 scoreMax = mismatch
-#
-# calculateMatrix(colChar,rowChar,0,0)
+
 
 
 
@@ -76,6 +71,11 @@ initMatrix(colChar,rowChar,0,0)
 #Create function for backtracking
 def backwards(seq1, seq2, x, y):
     return
+
+
+
+initMatrix(sequence1.get_sequence(),sequence2.get_sequence(),0,0)
+
 
 
 #print scoring matrix
